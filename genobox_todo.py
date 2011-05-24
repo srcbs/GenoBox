cc2f1d51e851d58e419ef143aeda8893b128abb2
Simon Rasmussen <simon@cbs.dtu.dk>
2011-05-24 15:42:16 +0200

    update todo

--- genobox_todo.py
+++ genobox_todo.py
@@ -5,3 +5,9 @@
 # move library stuff from genobox_modules so that it is inside the class
 
 genobox.py bcf2ref --bcf abcalls.all.bcf --genome /panvol1/simon/projects/aborigine_stinus/hg19_flat/build37_rCRS.genome --ex /panvol1/simon/databases/hs_ref37_rCRS/gi2number.build37_rCRS --dbsnp /panvol1/simon/databases/dbsnp/dbsnp132_hg19.vcf.gz --rmsk /panvol1/simon/databases/hs_ref37_rCRS/rmsk/rmsk_build37_rCRS.number.sort.genome --indels indels_for_filtering.number.vcf
+
+
+# example run
+
+# set variable first to make it easier, eg:
+set BUILD37=/panvol1/simon/databases/hs_ref37_rCRS
